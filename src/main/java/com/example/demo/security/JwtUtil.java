718bf03770b8a2fe1960641651de0db206b5f5b4
bala-amypo <bala@amypo.in>
2025-12-21 16:45:30 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,17 +1,34 @@
 package com.example.demo.security;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
-   
+    // Secret key for signing JWT
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    // Token validity: 1 hour
+    private static final long EXPIRATION_TIME = 60 * 60 * 1000;
+
+    /**
+     * Generates JWT token
+     */
+    public String generateToken(Long userId, String email, String role) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("userId", userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
 }
