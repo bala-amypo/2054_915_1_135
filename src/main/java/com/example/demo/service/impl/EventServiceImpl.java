b5b8a797dd35e6410f47ac9f98416e9a38178981
bala-amypo <bala@amypo.in>
2025-12-22 08:15:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EventServiceImpl.java
@@ -1,43 +1,31 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Event;
-import com.example.demo.entity.Role;
-import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.EventRepository;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.EventService;
-
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
-    private final UserRepository userRepository;
 
-    public EventServiceImpl(EventRepository eventRepository,
-                            UserRepository userRepository) {
+    public EventServiceImpl(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
-        this.userRepository = userRepository;
     }
 
     @Override
     public Event createEvent(Event event) {
-        User publisher = userRepository.findById(event.getPublisher().getId())
-                .orElseThrow();
-
-        if (publisher.getRole() != Role.PUBLISHER &&
-            publisher.getRole() != Role.ADMIN) {
-            throw new RuntimeException("Only PUBLISHER or ADMIN");
-        }
         return eventRepository.save(event);
     }
 
     @Override
     public Event updateEvent(Long id, Event event) {
         Event existing = eventRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Event not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
 
         existing.setTitle(event.getTitle());
         existing.setDescription(event.getDescription());
@@ -48,21 +36,20 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
-    public void deactivateEvent(Long id) {
-        Event event = eventRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Event not found"));
-        event.setActive(false);
-        eventRepository.save(event);
-    }
-
-    @Override
     public Event getById(Long id) {
         return eventRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Event not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
     }
 
     @Override
     public List<Event> getActiveEvents() {
         return eventRepository.findByIsActiveTrue();
+    }
+
+    @Override
+    public void deactivateEvent(Long id) {
+        Event event = getById(id);
+        event.setActive(false);
+        eventRepository.save(event);
     }
 }
