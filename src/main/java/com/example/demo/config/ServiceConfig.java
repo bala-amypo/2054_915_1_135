4be0b0ff8d51e2164f65fe11b6384e2fd5af4dd1
bala-amypo <bala@amypo.in>
2025-12-23 05:13:03 +0000

    User commit

--- src/main/java/com/example/demo/config/ServiceConfig.java
+++ src/main/java/com/example/demo/config/ServiceConfig.java
@@ -5,10 +5,16 @@ import com.example.demo.service.*;
 import com.example.demo.service.impl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public UserService userService(UserRepository userRepository,
