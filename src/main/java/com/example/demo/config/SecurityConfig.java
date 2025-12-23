37ad07d9870a9722d473de2abbd98aa83094fd98
bala-amypo <bala@amypo.in>
2025-12-23 04:55:02 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,62 +1,7 @@
 package com.example.demo.config;
 
-import com.example.demo.repository.UserRepository;
-import com.example.demo.security.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.*;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JwtUtil jwtUtil() {
-        return new JwtUtil(
-                "ThisIsAVerySecureSecretKeyForJwtDemo123456789",
-                3600000
-        );
-    }
-
-    @Bean
-    public CustomUserDetailsService userDetailsService(UserRepository repo) {
-        return new CustomUserDetailsService(repo);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,
-                                           JwtUtil jwtUtil) throws Exception {
-
-        http.csrf(csrf -> csrf.disable())
-            .sessionManagement(sm ->
-                    sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/swagger-ui/**",
-                            "/v3/api-docs/**",
-                            "/auth/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .exceptionHandling(ex ->
-                    ex.authenticationEntryPoint(
-                            new JwtAuthenticationEntryPoint()
-                    )
-            )
-            .addFilterBefore(
-                    new JwtAuthenticationFilter(jwtUtil),
-                    UsernamePasswordAuthenticationFilter.class
-            );
-
-        return http.build();
-    }
 }
