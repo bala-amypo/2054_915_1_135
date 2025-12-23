280a47a0d6e0aa87fd5dc088a351478847d9768e
bala-amypo <bala@amypo.in>
2025-12-23 05:19:02 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -3,7 +3,6 @@ package com.example.demo.security;
 public class JwtUtil {
 
     public JwtUtil(String secret, long validityInMs) {
-        // dummy
     }
 
     public String generateToken(Long userId, String email, String role) {
