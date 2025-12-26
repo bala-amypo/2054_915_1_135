eb8c81be9bae2bb1254d50bf7817445b6d896c28
bala-amypo <bala@amypo.in>
2025-12-26 04:53:46 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,12 +1,9 @@
 package com.example.demo.config;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,9 +11,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9069.408procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Digital Local Event Broadcasting API")
+                        .version("1.0.0"));
+        
+    }
 }
